bdf183a0d071012172b3a6ee52bcd7f0c782397e
Knut Flom Eriksen <156586369+Knut-Eriksen@users.noreply.github.com>
2024-11-06 21:49:54 +0100

    Used hash map insted of two loops

--- 0001-Two-Sum/0001-Two-Sum.cpp
+++ 0001-Two-Sum/0001-Two-Sum.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) { 
-        for (int i = 0;  i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+        
+    std::unordered_map<int, int> hashMap;
+        
+        for(int i = 0; i < nums.size(); i++){
+            int num = nums[i];
+            int complement = target - num;
+
+            if (hashMap.find(complement) != hashMap.end()){
+                return {hashMap[complement], i};
+            }
+            hashMap[num] = i; 
         }
-    }
     return {};
   }
 };
